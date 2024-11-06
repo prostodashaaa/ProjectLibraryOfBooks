93f1f27d9e0ca5f4a46e41a7cf5e21b433f13447
Darya <kozyreva2007sv@gmail.com>
2024-11-06 15:37:52 +0300

    Add files

---
+++ src/app.js
@@ -0,0 +1,30 @@
+import { FavoritesView } from "./views/favorites/favorites.js";
+import { MainView } from "./views/main/main.js";
+
+class App {
+  routes = [
+    { path: "", view: MainView },
+    { path: "#favorites", view: FavoritesView },
+  ];
+
+  appState = {
+    favorites: [],
+  };
+
+  constructor() {
+    window.addEventListener("hashchange", this.route.bind(this));
+    this.route();
+  }
+
+  route() {
+    if (this.currentView) {
+      this.currentView.destroy();
+    }
+
+    const view = this.routes.find((r) => r.path == location.hash).view;
+    this.currentView = new view(this.appState);
+    this.currentView.render();
+  }
+}
+
+new App();
