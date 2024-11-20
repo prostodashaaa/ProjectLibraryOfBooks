66ff1e0ec0ec54b2679c223228e89a67e5eab3ae
Darya <kozyreva2007sv@gmail.com>
2024-11-20 10:27:37 +0300

    fix

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -15,11 +15,11 @@ export class Header extends DivComponent {
 			</div>
 			<div class="menu">
 				<a class="menu__item" href="#">
-					<img src="./static/search.svg" alt="Поиск иконка" />
+					<img src="../static/search.svg" alt="Поиск иконка" />
 					Поиск книг
 				</a>
 				<a class="menu__item" href="#favorites">
-					<img src="./static/favorites.svg" alt="Избранное иконка" />
+					<img src="../static/favorites.svg" alt="Избранное иконка" />
 					Избранное
 					<div class="menu__counter">
 						${this.appState.favorites.length}
